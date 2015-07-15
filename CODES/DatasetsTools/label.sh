61a6f7504b8c5802f5f486c6cfd1f327a95c9bf9
zhuyafei <zhuyafei4520@163.com>
2015-07-15 21:57:16 +0800

    Modify label.sh to make sure that only .png format is dealt with

--- CODES/DatasetsTools/label.sh
+++ CODES/DatasetsTools/label.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 InputDatasets='./square'
+Extension='png'
 
 for m in $(ls $InputDatasets)
 do
@@ -12,34 +13,36 @@ do
 	do
 		for j in $(ls ${InputDatasets}/$m/$i)
 		do
-			printf ${InputDatasets}/$m/$i/$j'\t' >> $txtPath
-			printf $i'\t' >> $txtPath
-			if [ $i == Appendicularia ];then
-				printf '1\n' >> $txtPath
-			elif [ $i == Bubble ];then
-				printf '2\n' >> $txtPath
-			elif [ $i == Chaetognatha ];then
-				printf '3\n' >> $txtPath
-			elif [ $i == CladoceraPenilia ];then
-				printf '4\n' >> $txtPath
-			elif [ $i == Copepoda ];then
-				printf '5\n' >> $txtPath
-			elif [ $i == Decapoda ];then
-				printf '6\n' >> $txtPath
-			elif [ $i == Doliolida ];then
-				printf '7\n' >> $txtPath
-			elif [ $i == Egg ];then
-				printf '8\n' >> $txtPath
-			elif [ $i == Fiber ];then
-				printf '9\n' >> $txtPath
-			elif [ $i == Gelatinous ];then
-				printf '10\n' >> $txtPath
-			elif [ $i == Multiple ];then
-				printf '11\n' >> $txtPath
-			elif [ $i == Nonbio ];then
-				printf '12\n' >> $txtPath
-			else 
-				printf '13\n' >> $txtPath
+			if [ ${j##*.} == ${Extension} ];then
+				printf ${InputDatasets}/$m/$i/$j'\t' >> $txtPath
+				printf $i'\t' >> $txtPath
+				if [ $i == Appendicularia ];then
+					printf '1\n' >> $txtPath
+				elif [ $i == Bubble ];then
+					printf '2\n' >> $txtPath
+				elif [ $i == Chaetognatha ];then
+					printf '3\n' >> $txtPath
+				elif [ $i == CladoceraPenilia ];then
+					printf '4\n' >> $txtPath
+				elif [ $i == Copepoda ];then
+					printf '5\n' >> $txtPath
+				elif [ $i == Decapoda ];then
+					printf '6\n' >> $txtPath
+				elif [ $i == Doliolida ];then
+					printf '7\n' >> $txtPath
+				elif [ $i == Egg ];then
+					printf '8\n' >> $txtPath
+				elif [ $i == Fiber ];then
+					printf '9\n' >> $txtPath
+				elif [ $i == Gelatinous ];then
+					printf '10\n' >> $txtPath
+				elif [ $i == Multiple ];then
+					printf '11\n' >> $txtPath
+				elif [ $i == Nonbio ];then
+					printf '12\n' >> $txtPath
+				else 
+					printf '13\n' >> $txtPath
+				fi
 			fi
 		done
 	done
