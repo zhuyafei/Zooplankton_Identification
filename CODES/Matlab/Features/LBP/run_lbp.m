60ebacd7faa1232ffd8dc67fa9f824b2e8a4c381
zhuyafei <zhuyafei4520@163.com>
2015-07-17 15:40:02 +0800

    Change the structure of CODES

---
+++ CODES/Matlab/Features/LBP/run_lbp.m
@@ -0,0 +1,67 @@
+clear
+clc
+TrainingSetInfo = importdata('Training_Set.txt');
+TestSetInfo = importdata('Test_Set.txt');
+TrainingSetNum = length(TrainingSetInfo.data);
+TestSetNum = length(TestSetInfo.data);
+
+%% For random dividing - cross validation
+fid = fopen('Train-Features-LBP.txt','w');
+for i = 1:TrainingSetNum
+    img = single(imread(TrainingSetInfo.textdata{i, 1}));
+    img = imresize(img, [256 256]);
+    lbpmatrix = vl_lbp(img, 32);
+    lbpvector = lbpmatrix(:)';
+    for j = 1:length(lbpvector)
+        fprintf(fid, '%g\t', lbpvector(j));
+    end
+    fprintf(fid, '\n');
+end
+fclose(fid);
+
+fid = fopen('Test-Features-LBP.txt','w');
+for i = 1:TestSetNum
+    img = single(imread(TestSetInfo.textdata{i, 1}));
+    img = imresize(img, [256 256]);
+    lbpmatrix = vl_lbp(img, 32);
+    lbpvector = lbpmatrix(:)';
+    for j = 1:length(lbpvector)
+        fprintf(fid, '%g\t', lbpvector(j));
+    end
+    fprintf(fid, '\n');
+end
+fclose(fid);
+
+%% For average dividing - cross validation
+% fid = fopen('Train-Features-LBP.txt','w');
+% for i = 1:TrainingSetNum
+%     img = single(imread(TrainingSetInfo.textdata{i, 1}));
+%     img = imresize(img, [256 256]);
+%     lbpmatrix = vl_lbp(img, 32);
+%     lbpvector = lbpmatrix(:)';
+%     fprintf(fid, '%g\t', TrainingSetInfo.data(i));
+%     for j = 1:length(lbpvector)
+%         if lbpvector(j) ~= 0
+%             fprintf(fid, '%g:%g\t', j, lbpvector(j));
+%         end
+%     end
+%     fprintf(fid, '\n');
+% end
+% fclose(fid);
+% 
+% fid = fopen('Test-Features-LBP.txt','w');
+% for i = 1:TestSetNum
+%     img = single(imread(TestSetInfo.textdata{i, 1}));
+%     img = imresize(img, [256 256]);
+%     lbpmatrix = vl_lbp(img, 32);
+%     lbpvector = lbpmatrix(:)';
+%     fprintf(fid, '%g\t', TestSetInfo.data(i));
+%     for j = 1:length(lbpvector)
+%         if lbpvector(j) ~= 0
+%             fprintf(fid, '%g:%g\t', j, lbpvector(j));
+%         end
+%     end
+%     fprintf(fid, '\n');
+% end
+% fclose(fid);
+
