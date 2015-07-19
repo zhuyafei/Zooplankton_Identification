02594be571e74374522fa796baa610e17ef7d20b
zhuyafei <zhuyafei4520@163.com>
2015-07-19 17:49:58 +0800

    Modify run_hog.m

--- CODES/Matlab/Features/HOG/run_hog.m
+++ CODES/Matlab/Features/HOG/run_hog.m
@@ -6,59 +6,59 @@ TrainingSetNum = length(TrainingSetInfo.data);
 TestSetNum = length(TestSetInfo.data);
 
 %% For random dividing - cross validation
-fid = fopen('Train-Features-LBP.txt','w');
+fid = fopen('Train-Features-HOG.txt','w');
 for i = 1:TrainingSetNum
     img = single(imread(TrainingSetInfo.textdata{i, 1}));
     img = imresize(img, [256 256]);
-    lbpmatrix = vl_hog(img, 32);
-    lbpvector = lbpmatrix(:)';
-    for j = 1:length(lbpvector)
-        fprintf(fid, '%g\t', lbpvector(j));
+    hogmatrix = vl_hog(img, 32);
+    hogvector = hogmatrix(:)';
+    for j = 1:length(hogvector)
+        fprintf(fid, '%g\t', hogvector(j));
     end
     fprintf(fid, '\n');
 end
 fclose(fid);
 
-fid = fopen('Test-Features-LBP.txt','w');
+fid = fopen('Test-Features-HOG.txt','w');
 for i = 1:TestSetNum
     img = single(imread(TestSetInfo.textdata{i, 1}));
     img = imresize(img, [256 256]);
-    lbpmatrix = vl_hog(img, 32);
-    lbpvector = lbpmatrix(:)';
-    for j = 1:length(lbpvector)
-        fprintf(fid, '%g\t', lbpvector(j));
+    hogmatrix = vl_hog(img, 32);
+    hogvector = hogmatrix(:)';
+    for j = 1:length(hogvector)
+        fprintf(fid, '%g\t', hogvector(j));
     end
     fprintf(fid, '\n');
 end
 fclose(fid);
 
 %% For average dividing - cross validation
-% fid = fopen('Train-Features-LBP.txt','w');
+% fid = fopen('Train-Features-HOG.txt','w');
 % for i = 1:TrainingSetNum
 %     img = single(imread(TrainingSetInfo.textdata{i, 1}));
 %     img = imresize(img, [256 256]);
-%     lbpmatrix = vl_hog(img, 32);
-%     lbpvector = lbpmatrix(:)';
+%     hogmatrix = vl_hog(img, 32);
+%     hogvector = hogmatrix(:)';
 %     fprintf(fid, '%g\t', TrainingSetInfo.data(i));
-%     for j = 1:length(lbpvector)
-%         if lbpvector(j) ~= 0
-%             fprintf(fid, '%g:%g\t', j, lbpvector(j));
+%     for j = 1:length(hogvector)
+%         if hogvector(j) ~= 0
+%             fprintf(fid, '%g:%g\t', j, hogvector(j));
 %         end
 %     end
 %     fprintf(fid, '\n');
 % end
 % fclose(fid);
 % 
-% fid = fopen('Test-Features-LBP.txt','w');
+% fid = fopen('Test-Features-HOG.txt','w');
 % for i = 1:TestSetNum
 %     img = single(imread(TestSetInfo.textdata{i, 1}));
 %     img = imresize(img, [256 256]);
-%     lbpmatrix = vl_hog(img, 32);
-%     lbpvector = lbpmatrix(:)';
+%     hogmatrix = vl_hog(img, 32);
+%     hogvector = hogmatrix(:)';
 %     fprintf(fid, '%g\t', TestSetInfo.data(i));
-%     for j = 1:length(lbpvector)
-%         if lbpvector(j) ~= 0
-%             fprintf(fid, '%g:%g\t', j, lbpvector(j));
+%     for j = 1:length(hogvector)
+%         if hogvector(j) ~= 0
+%             fprintf(fid, '%g:%g\t', j, hogvector(j));
 %         end
 %     end
 %     fprintf(fid, '\n');
